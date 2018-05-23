bc498a5e963608d44a1d95dbad88fcab2bfdc88f
Eric McNamara <Nishcov@gmail.com>
2018-05-23 11:48:23 -0600

    Added Models for Person, Address, and collection of Interests

---
+++ PeopleSearch/Models/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string Locality { get; set; }
+
+        public string Province { get; set; }
+
+        public int PostCode { get; set; }
+
+        public string Country { get; set; }
+
+        public Person Person { get; set; }
+
+        public int PersonId { get; set; }
+    }
+}
---
+++ PeopleSearch/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+
+        public int Age { get; set; }
+
+        public ICollection<Interest> Interests { get; set; }
+
+        public Person()
+        {
+            Interests = new Collection<Interest>();
+        }
+    }
+}
