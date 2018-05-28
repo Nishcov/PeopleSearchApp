8de478b2544dabcdaef3abeb88ecdcaba37cd511
Eric McNamara <Nishcov@gmail.com>
2018-05-28 10:16:05 -0600

    Updated Interests to seeded database records.

---
+++ PeopleSearch/Migrations/20180528160536_InsertInterests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PeopleSearch.Migrations
+{
+    public partial class InsertInterests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE People SET Interests = 'Movies, TV' WHERE Id = (SELECT Id FROM People WHERE FirstName = 'John' AND LastName = 'Doe')");
+            migrationBuilder.Sql("UPDATE People SET Interests = 'Books, stargazing' WHERE Id = (SELECT Id FROM People WHERE FirstName = 'Jane' AND LastName = 'Doe')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
